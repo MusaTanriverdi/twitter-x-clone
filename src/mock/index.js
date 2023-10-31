4786cefb865a0ca8734922a3d26ed070494a6988
Musa Tanrıverdi <musatanriiverdii@gmail.com>
2023-10-31 18:32:11 +0300

    right sidebar

---
+++ src/mock/index.js
@@ -0,0 +1,21 @@
+export const whoFollowUsers = [
+    {
+        id: 3,
+        username: 'MusaDev',
+        fullName: 'Musa Tanriverdi',
+        avatar: 'https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg',
+    },
+    {
+        id: 4,
+        username: 'Admin',
+        fullName: 'Musa',
+        avatar: 'https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg',
+    },
+    {
+        id: 5,
+        username: 'tester22',
+        fullName: 'tester',
+        avatar: 'https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg',
+        verified: true
+    }
+]
